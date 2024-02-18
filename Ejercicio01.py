fb18fab4238eabd3ce4e076df138841deff2556e
Alexander <Alexarroyo01040@gmail.com>
2024-02-18 09:04:59 -0500

    ADD script python

---
+++ Ejercicio01.py
@@ -0,0 +1,11 @@
+# INGRESAR USUARIO
+nombre= input("ingrese su nombre")
+edad= int(input("ingrese su edad"))
+
+# CALCULO DEL EJECICIO
+base = float(input("Ingrese la longitud de la base del triángulo: "))
+altura = float(input("Ingrese la altura del triángulo: "))
+area = (base * altura) // 2
+
+# Resultado
+print(f"El área del triángulo con base {base} y altura {altura} es: {area}")
